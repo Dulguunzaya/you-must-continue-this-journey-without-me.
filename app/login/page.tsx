1f1ade0615832d2cd044771f8dcb0713d16c692a
Dulguunzaya <dukmongonshagai@gmail.com>
2026-01-03 15:55:18 +0800

    no-error

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -7,8 +7,8 @@ const page = () => {
       <div className="w-[500px] h-[650px] bg-gray-400 border-2 border-black rounded-lg">
         <div className="text-center text-3xl pt-6">Automated Billiard</div>
         <div className="flex justify-center items-center gap-5 mt-10">
-          <Button name="Login" color="blue" link="/login"/>
-          <Button name="Register" color="gray" link="/register"/>
+          <Button name="Login" color="blue" />
+          <Button name="Register" color="gray" />
         </div>
       </div>
     </div>
