01b3349e99fa450c7a57dfc6ce360c1e6f0e3956
Dulguunzaya <dukmongonshagai@gmail.com>
2026-01-09 02:54:20 +0800

    ...

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,16 @@
-import DashboardPage from "./dashboard/page";
+import React from "react";
 
-const Page = () => {
-  return <DashboardPage />;
+const page = () => {
+  return (
+    <div className="flex justify-center items-center h-screen cursor-pointer">
+      <a
+        href="https://github.com/Dulguunzaya/Billiard"
+        className="bg-blue-300 text-black p-2 rounded-2xl hover:bg-blue-500 transition-all ease-in-out duration-300 "
+      >
+        <button>103 111 111 100 98 121 101</button>
+      </a>
+    </div>
+  );
 };
 
-export default Page;
+export default page;
